cc36ec7e23db25f6f7695ffcf74ef6396fb98baa
gusCreator <lgsc21211@gmail.com>
2024-08-02 23:11:11 -0500

    Link for each developer account has been added.

--- knowledge-wardens/src/components/ui/home/Developer.tsx
+++ knowledge-wardens/src/components/ui/home/Developer.tsx
@@ -28,23 +28,26 @@ export function Developer({ username, trueName, shortDescription }: DeveloperPro
   }, [username]);
 
   return (
-    <article className="flex flex-col items-center justify-center gap-3 p-4 w-60">
-       <div className="relative w-44 h-44 rounded-full overflow-hidden flex-shrink-0">
-        {loading ? (
-          <div className="absolute inset-0 bg-gray-200 animate-pulse rounded-full"></div>
-        ) : (
-          <Image
-            className="object-cover"
-            fill
-            src={imgUser}
-            alt={`${username}'s avatar`}
-          />
-        )}
-      </div>
-      <div className="flex flex-col flex-1 gap-1">
-        <h5 className="text-xl font-semibold text-center">{trueName ? trueName : username}</h5>
-        <p className="text-center">{shortDescription}</p>
-      </div>
+    <article className="p-4 w-60">
+      <a className="flex flex-col items-center gap-3" href={`https://github.com/${username}`}>
+        <div className="relative w-44 h-44 rounded-full overflow-hidden flex-shrink-0">
+          {loading ? (
+            <div className="absolute inset-0 bg-gray-200 animate-pulse rounded-full"></div>
+          ) : (
+            <Image
+              className="object-cover"
+              fill
+              src={imgUser}
+              alt={`${username}'s avatar`}
+            />
+          )}
+        </div>
+        <div className="flex flex-col flex-1 gap-1">
+          <h5 className="text-xl font-semibold text-center">{trueName ? trueName : username}</h5>
+          <p className="text-center">{shortDescription}</p>
+        </div>
+      </a>
+
     </article >
   )
 
