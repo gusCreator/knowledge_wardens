dfce5b9a78fb41978731e5e5328a6e1aa1b8f425
gusCreator <lgsc21211@gmail.com>
2024-08-03 23:00:25 -0500

    Some style and content has been added to the page. The new components creation is planed.

--- knowledge-wardens/src/components/ui/home/Home.tsx
+++ knowledge-wardens/src/components/ui/home/Home.tsx
@@ -19,7 +19,12 @@ export function Home({ session }: HomeProps) {
   return (
     <>
       <header className="flex flex-col justify-between items-center bg-main-image bg-cover bg-center min-h-[800px] max-h-full p-8">
-        <h1 className="text-center text-5xl lg:text-7xl xl-text-7xl pt-5 text-yellow-600 font-extrabold">Knowledge Wardens</h1>
+        <h1 className="text-center text-5xl lg:text-7xl xl:text-7xl pt-5 text-yellow-600 font-extrabold">
+          Knowledge Wardens
+        </h1>
+        <h3 className="text-gray-300 text-xl lg:text-2xl xl:text-2xl w-2/3 bg-gray-900 bg-opacity-75 bg- rounded-3xl p-4">
+          The powerful witch wizard has possessed your master and there is only one way to defeat him. You must overcome their ancient knowledge. A legend is your hope, look for knowledge wardens and achieve the victory.
+        </h3>
         <div className="p-8 bg-gray-900 bg-opacity-75 w-fit rounded-3xl border-yellow-500 border-4">
           {
             !session && <Login />
@@ -32,10 +37,18 @@ export function Home({ session }: HomeProps) {
       </header>
       <main>
         {/* Authors section */}
+        <section>
+          <h3>
+            Learn and win
+          </h3>
+          <p>
+            The powerful witch wizard has defeated and possessed your master and there is only one way to defeat him. You must overcome their ancient knowledge. A legend is your hope, it is said that a group of wardens lives in their temple in the forest, look for them and start your train to achieve victory.
+          </p>
+        </section>
         <Developers />
       </main>
-      <footer className="flex justify-center">
-        <a href="https://github.com/gusCreator/knowledge_wardens" className="flex items-center gap-1"><FaGithub size={20} /> Visit repository</a>
+      <footer className="flex justify-center bg-black p-5">
+        <a href="https://github.com/gusCreator/knowledge_wardens" className="flex items-center gap-1 text-white"><FaGithub size={20} /> Visit repository</a>
       </footer>
     </>
   )
