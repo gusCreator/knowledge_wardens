5669da20dc30dbdc7e65890f1339c3301e635a2d
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-08-03 22:09:10 -0500

    Falta importar el token para la base de datos

--- knowledge-wardens/src/app/api/courses/route.ts
+++ knowledge-wardens/src/app/api/courses/route.ts
@@ -1,5 +1,7 @@
+import exp from "constants";
 import { NextApiRequest, NextApiResponse } from "next";
 
+//const apiURL = process.env.NEXTAUTH_URL
 //export default function handler(req: NextApiRequest, res: NextApiResponse) {
 //  if (req.method === 'GET') {
 //    const courses = [
@@ -20,16 +22,8 @@ export async function GET(req: NextApiRequest, res: NextApiResponse) {
     id: 'xd'
   }
   return Response.json(data);
-  //if (req.method === 'GET') {
-  //  const courses = [
-  //    { name: 'Hola Mundo' }
-  //  ];
-  //  res.status(200).json(courses);
-  //} else if ( req.method === 'POST' ) {
-  //  const newCourse = req.body;
-  //  res.status(201).json(newCourse);
-  //} else {
-  //  res.setHeader('Allow', [ 'GET', 'POST' ]);
-  //  res.status(405).end(`Method ${req.method} Not allowed`);
-  //}
+}
+
+export async function POST(request: Request) {
+
 }
