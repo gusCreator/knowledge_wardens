c38a9713193cf7e690d9589182493660d4b0050f
gusCreator <lgsc21211@gmail.com>
2024-08-05 20:15:04 -0500

    It was asked to IA that explain the topic of the course. Also, tha IA decides the number of questions.

--- knowledge-wardens/src/app/api/ai/courses/route.ts
+++ knowledge-wardens/src/app/api/ai/courses/route.ts
@@ -50,9 +50,9 @@ export async function POST(res: Request) {
   const response = await generateObject({
     model,
     schema: z.object({
-      subthemes: z.array(z.string().describe('A content subtheme for the topic contein 60 a 70 words. You need to explain this subtheme in detail. Provide the response in plain text.')),
+      subthemes: z.array(z.string().describe('A content subtheme for the topic. You need to explain this subtheme in detail like a teacher. Provide the response in plain text.')),
     }),
-    system: 'You generate six subthemes for the topic. Choose the most relevant and practical topics',
+    system: 'You generate subthemes for the topic and explain them in detail. Choose the most relevant and practical topics, the subthemes have to be enough to learn the course completely.',
     prompt: `The course is ${validatedRequest.data.course}, the topic is ${validatedRequest.data.topicName}, and the description is ${validatedRequest.data.topicDescription}. Respond in Spanish please.`,
   });
 
